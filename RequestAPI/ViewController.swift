b0499b876c83bc1245adf030166b2ab39cf44dbf
vietdo1701 <leoasula@gmail.com>
2022-08-31 19:42:22 +0700

    Initial Commit

---
+++ RequestAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RequestAPI
+//
+//  Created by leo asula on 31/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
